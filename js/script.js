119027d9adfe8c7a06c48396cbf2a04494e928a5
Esteban Villada <evilladah5@gmail.com>
2022-06-21 04:04:48 -0500

    Encendedor sin animacion

--- js/script.js
+++ js/script.js
@@ -1,17 +1,42 @@
+var numero =0;
+
+function dioClick(){
+    var body = document.getElementById("body");    
+    let bulb = document.querySelector("#light");    
+        if(esPar(numero)){
+             body.style.backgroundColor ="black";
+             bulb.classList.add("on");
+                 for(i = 0 ; i<document.getElementsByClassName("item").length ; i++){
+                      document.getElementsByClassName("item")[i].style.display="block"
+                         }        } 
+        else { 
+            body.style.backgroundColor ="#f7f7f7";
+            bulb.classList.remove("on");
+                for(i = 0 ; i<document.getElementsByClassName("item").length ; i++){
+                        document.getElementsByClassName("item")[i].style.display="none"
+                        }           }
+numero++;
+
+}
+
+
+/*
 function bombillo(x){
     console.log(x.value);
     var body = document.getElementById("body");
     body.style.backgroundColor =x.value;
-    var css = document.getElementById("text");
-    var css1 = document.getElementById("text1");
-    css.style.display="block";
     let bulb = document.querySelector("#light");
     if(x.value == "#f7f7f7"){bulb.classList.remove("on");
-    css.style.display="none";
-    css1.style.display="none";}    
-    else{
-    bulb.classList.add("on");
-    css.style.display="block";
-    css1.style.display="block";}}
-   
+    for(i = 0 ; i<document.getElementsByClassName("item").length ; i++){
+        document.getElementsByClassName("item")[i].style.display="none"
+    }}    
+    else{bulb.classList.add("on");
+    for(i = 0 ; i<document.getElementsByClassName("item").length ; i++){
+        document.getElementsByClassName("item")[i].style.display="block"
+    }}}
+*/
+    function esPar(numero)
+    {
+    return (numero % 2) == 0;
+    }
       
