7dabb37dd79dc6344628cde74af0ceae803f8f44
Esteban Villada <evilladah5@gmail.com>
2022-06-21 17:19:40 -0500

    encendido y apagado gradual

--- js/script.js
+++ js/script.js
@@ -3,16 +3,18 @@ var numero =0;
 function dioClick(){
     var body = document.getElementById("body");    
     let bulb = document.querySelector("#light");
+    let bulbShadow =document.getElementsByClassName("bulb");
         if(esPar(numero)){
             document.getElementById("encendedor").style.color="#f7f7f7";
-             body.style.backgroundColor ="black";
+             body.style="--myColor1: #FFFFFF ; --myColor2: 	#000000;";
+             //body.style ="radial-gradient( farthest-side circle at center top, #f7f7f7, #000 )";
              bulb.classList.add("on");
                  for(i = 0 ; i<document.getElementsByClassName("item").length ; i++){
                       document.getElementsByClassName("item")[i].style.display="block"
                          }        } 
         else { 
             document.getElementById("encendedor").style.color="black";
-            body.style.backgroundColor ="#f7f7f7";
+            body.style="--myColor1: #000000 ; --myColor2: 	#000000;"
             bulb.classList.remove("on");
                 for(i = 0 ; i<document.getElementsByClassName("item").length ; i++){
                         document.getElementsByClassName("item")[i].style.display="none"
