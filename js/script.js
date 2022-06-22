44132299874d1ea1e6f9c3200267ca26813284d5
Esteban Villada <evilladah5@gmail.com>
2022-06-21 19:55:43 -0500

    efecto de sonido y animacion

--- js/script.js
+++ js/script.js
@@ -1,28 +1,31 @@
 var numero =0;
-
+var sonido = new Audio();
+sonido.src = "./src/sound.mp3";
 function dioClick(){
+    
+    sonido.play();
     var body = document.getElementById("body");    
     let bulb = document.querySelector("#light");
-    let bulbShadow =document.getElementsByClassName("bulb");
         if(esPar(numero)){
             document.getElementById("encendedor").style.color="#f7f7f7";
-             body.style="--myColor1: #FFFFFF ; --myColor2: 	#000000;";
-             //body.style ="radial-gradient( farthest-side circle at center top, #f7f7f7, #000 )";
+             body.style="--myColor1: #CBCBCB ; --myColor2: 	#000000;";
              bulb.classList.add("on");
                  for(i = 0 ; i<document.getElementsByClassName("item").length ; i++){
                       document.getElementsByClassName("item")[i].style.display="block"
-                         }        } 
+                         }   } 
         else { 
             document.getElementById("encendedor").style.color="black";
             body.style="--myColor1: #000000 ; --myColor2: 	#000000;"
             bulb.classList.remove("on");
                 for(i = 0 ; i<document.getElementsByClassName("item").length ; i++){
                         document.getElementsByClassName("item")[i].style.display="none"
-                        }           }
+                        }       
+                    }
 numero++;
-
 }
-
+function esPar(numero){
+return (numero % 2) == 0;
+}
 
 /*
 function bombillo(x){
@@ -39,8 +42,5 @@ function bombillo(x){
         document.getElementsByClassName("item")[i].style.display="block"
     }}}
 */
-    function esPar(numero)
-    {
-    return (numero % 2) == 0;
-    }
+
       
